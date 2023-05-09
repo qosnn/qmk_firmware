7badf6af7f60ac312c8ebdcb68f55b2232c2b74f
qosnn <nmqos2c@gmail.com>
2023-05-09 19:52:24 +0300

    adding hv_kb390l new project structure

--- keyboards/hv_kb390l/hv_kb390l.c
+++ keyboards/hv_kb390l/hv_kb390l.c
@@ -1,12 +1,17 @@
+/* Copyright 2023 QMK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "hv_kb390l.h"
-
-void bootmagic_lite(void) {
-    matrix_scan();
-    wait_ms(DEBOUNCE * 2);
-    matrix_scan();
-
-    if (matrix_get_row(BOOTMAGIC_LITE_ROW) & (1 << BOOTMAGIC_LITE_COLUMN)) {
-      // Jump to bootloader.
-      bootloader_jump();
-    }
-}
